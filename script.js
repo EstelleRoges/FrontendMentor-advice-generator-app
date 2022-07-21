971b9a12b0d7a86d6639edea4389332cb038d9c2
EstelleR <roges.estelle@gmail.com>
2022-07-21 12:31:26 +0200

    script put in another file - code updated

---
+++ script.js
@@ -0,0 +1,32 @@
+let adviceId = document.getElementById("adviceId");
+    let advice = document.getElementById("advice");
+    let button = document.getElementById("adviceButton");
+
+    const fetchAdvice = () => {
+    const random = Math.floor(Math.random() * 200);
+    console.log(random);
+      fetch(`https://api.adviceslip.com/advice/${random}`).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        adviceId.innerHTML = data.slip.id;
+        advice.innerText = data.slip.advice;
+        advice.classList.add("fadeIn");
+        
+        button.addEventListener('click', function() {
+          button.classList.add("disabled");
+          button.style.cursor ="not-allowed";
+        });
+
+        setTimeout(function() {
+          advice.classList.remove("fadeIn");
+        }, 800);
+
+        setTimeout(function() {
+          button.classList.remove("disabled");
+          button.style.cursor = "pointer";
+        }, 2000);
+
+      }).catch(function (error) {
+        console.warn("Failed to get an advice", error);
+      });
+    }
